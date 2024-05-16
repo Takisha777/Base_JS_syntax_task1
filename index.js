c2bd6898a08c7a7252760c95789828ec398442ad
Takisha777 <alexey.kovtun.od@gmail.com>
2024-05-16 02:07:10 +0200

    Base_JS_syntax_task1

---
+++ index.js
@@ -0,0 +1,15 @@
+//JavaScript-код с созданием переменных
+/*  Автор:
+    Alexey Kovtun
+ */
+const myName = 'Alex',
+    mySurname = 'Kovtun';
+let myFavoriteDrink = 'water',
+    myFavoriteAnimal = 'cat',
+    myFavoriteProgrammingLanguage = 'JavaScript';
+
+console.log(myName);
+console.log(mySurname);
+console.log(myFavoriteDrink);
+console.log(myFavoriteAnimal);
+console.log(myFavoriteProgrammingLanguage);
